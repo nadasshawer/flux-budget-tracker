a8b79986de893babce4633bf63c65ef2836817e0
Nada Shawer <nadashawer2023@gmail.com>
2026-04-26 15:46:57 -0700

    Remove build directory from tracking

--- build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.obj: \
- C:\Users\nadas\Desktop\programming\C++\finance_tracker_refined\build\_deps\ftxui-src\src\ftxui\dom\flexbox_config.cpp \
- C:/Users/nadas/Desktop/programming/C++/finance_tracker_refined/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
